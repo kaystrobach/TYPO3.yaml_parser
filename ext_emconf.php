104453fdff450b0f70a85344c27c077b23df2265
Kay Strobach <info@kay-strobach.de>
2014-09-24 11:52:34 +0200

    [TASK] Fix autoloader with enabled cache

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "Themes".
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Symfony YAML Parser for 6.2',
+	'description' => '',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Kay Strobach',
+	'author_email' => 'typo3@kay-strobach.de',
+	'author_company' => 'private',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '6.2.0-6.2.99'
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,3 @@
+<?php
+
+include_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('yaml_parser') . '/Resources/Private/PHP/vendor/autoload.php');
